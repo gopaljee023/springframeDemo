d166c08361726b07c34d4a4349d70510a68b5c51
gopaljee <gopaljee023@gmail.com>
2020-04-02 07:41:12 +0530

    constructor-arg : constructor injection

--- src/com/jee/Triangle.java
+++ src/com/jee/Triangle.java
@@ -3,13 +3,28 @@ package com.jee;
 public class Triangle {
 
 	String type;
+	private int height;
+	
+	public Triangle(String type) {
+		this.type = type;
+	}
+	
+	
+	public Triangle(int height) {
+		this.height = height;
+		System.out.println("not called");
+	}
+	
+	
+	public Triangle(String type,int height) {
+		this.type = type;
+		this.height =height;
+	}
 	public String getType() {
 		return type;
 	}
-	public void setType(String type) {
-		this.type = type;
-	}
+	
 	public void draw() {
-		System.out.println(type.toString()+" Triangle draw");
+		System.out.println(type.toString()+" Triangle draw with height "+ height);
 	}
 }
